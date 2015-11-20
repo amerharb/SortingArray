b28a65b2db13006999c512e5fff132b8926ce8d5
amerharb <amer.harb@gmail.com>
2015-11-20 14:31:37 +0100

    UPDATE Task4 binary search

--- src/seminar1/BinarySearch.java
+++ src/seminar1/BinarySearch.java
@@ -14,30 +14,116 @@ import java.util.List;
 public class BinarySearch
 {
 
-    class element{
+    class element
+    {
+
         int OriginalOrder;
         int value;
     }
 
-    public static int binarySearch(int[] a, int x) throws Exception
+    public static int binarySearch(int[] a, int x) 
     {
+        element[] eList = new element[a.length];
+
+        for (element e : eList) {
+            int i = 0;
+            e.value = a[i];
+            e.OriginalOrder = i;
+            i++;
+        }
+        
+        sortElement(eList);
+        
+        return eList[binarySearchOrderedArray(eList, x)].OriginalOrder;
+    }
+
+    private static void sortElement(element[] items)
+    {
+        if (items.length > 1) {
+            element[] tempSmaller = new element[items.length];
+            element[] tempSame = new element[items.length];
+            element[] tempLarger = new element[items.length];
+
+            int smallerCounter = 0, sameCounter = 0, largerCounter = 0;
+            
+            element chosenItem = items[items.length / 2];
+            for (int i = 0; i < items.length; i++) {
+                if (items[i].value < chosenItem.value) {
+                    tempSmaller[smallerCounter].value = items[i].value;
+                    smallerCounter++;
+                } else if (items[i].value > chosenItem.value) {
+                    tempLarger[largerCounter] = items[i];
+                    largerCounter++;
+                } else {
+                    tempSame[sameCounter] = items[i];
+                    sameCounter++;
+                }
+            }
+            
+            element[] smaller = new element[smallerCounter];
+            for (int i = 0; i < smallerCounter; i++) {
+                smaller[i] = tempSmaller[i];
+            }
+            tempSmaller = null;
+            
+            element[] same = new element[sameCounter];
+            for (int i = 0; i < sameCounter; i++) {
+                same[i] = tempSame[i];
+            }
+            tempSame = null;
+            
+            element[] larger = new element[largerCounter];
+            for (int i = 0; i < largerCounter; i++) {
+                larger[i] = tempLarger[i];
+            }
+            tempLarger = null;
+            
+            sortElement(smaller); // Recursive call!
+            sortElement(larger); // Recursive call!
+
+            int x = 0;
+            
+            for (element i : smaller) {
+                items[x] = i;
+                x++;
+            }
+            smaller = null;
+            
+            for (element i : same) {
+                items[x] = i;
+                x++;
+            }
+            same = null;
+            
+            for (element i : larger) {
+                items[x] = i;
+                x++;
+            }
+            larger = null;
+
+        }
+    }
+
+    private static int binarySearchOrderedArray(element[] a, int x)
+    {
+
         int low = 0, high = a.length - 1;
 
         while (low <= high) {
             int mid = (low + high) / 2;
 
-            if (a[mid] < x) {
+            if (a[mid].value < x) {
                 low = mid + 1;
-            } else if (a[mid] > x) {
+            } else if (a[mid].value > x) {
                 high = mid - 1;
             } else {
                 return mid; // Found
             }
         }
-        throw new Exception(); // NOT_FOUND is defined as -1
+        return -1; // NOT_FOUND is defined as -1
     }
 
-    public static Integer binarySearch(List<Integer> a, int x)
+    private static Integer binarySearch(List<Integer> a, int x)
     {
 
         int low = 0, high = a.size() - 1;
