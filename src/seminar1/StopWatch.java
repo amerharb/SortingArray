5fac2868a893f074eb324cc84334cb710c37b0f3
amerharb <amer.harb@gmail.com>
2015-11-22 21:25:48 +0100

    update StopWatch

--- src/seminar1/StopWatch.java
+++ src/seminar1/StopWatch.java
@@ -1,45 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package seminar1;
 
-/**
- *
- * @author Amer
- */
 public class StopWatch
 {
 
     private long period;
     private long startTime;
-//    private long stopTime;
     private byte status; //0- stoped, 1-running
 
-    public StopWatch()
+    public void start()
     {
-        period = 0;
+        startTime = System.nanoTime();
+        status = 1;
     }
 
     public void stop()
     {
-        //period += (System.currentTimeMillis() - startTime);
         period += (System.nanoTime() - startTime);
         status = 0;
-    }
-
-    public void start()
-    {
-        //startTime = System.currentTimeMillis();
-        startTime = System.nanoTime();
-        status = 1;
     }
 
     public void reset()
     {
         period = 0;
-        //startTime = System.currentTimeMillis();
         startTime = System.nanoTime();
     }
 
@@ -49,10 +31,8 @@ public class StopWatch
         case 0:
             return period;
         case 1:
-            //return period + (System.currentTimeMillis() - startTime);
             return period + (System.nanoTime() - startTime);
         }
-
         return period;
     }
 
